cb42e3f9bbe2dc2a780ca067bd1a0cd14ce2c32f
Brian Felix <fbrian100@gmail.com>
2023-06-01 13:44:19 -0400

    Initial draft
    Mostly Quarterly information

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# opera-wiki
-Opera Open Source Wiki to share documentation with the community
+# Opera Wiki
+
+This project contains the Opera Wiki. This was forked from the [Friends with Benefits Wiki](https://github.com/friends-with-benefits/fwb-wiki)
+
+## Nextra
+The project is based on [Nexta](https://nextra.vercel.app), a [Next.js](https://nextjs.org) plugin for serving MD and [MDX](https://mdxjs.com) files. Nextra is created by [@shuding](https://github.com/shuding) and [@pacocoursey](https://github.com/pacocoursey) at [Vercel](https://vercel.com). Released under the MIT license.
---
+++ theme.config.jsx
@@ -0,0 +1,20 @@
+export default {
+  logo: <span>Opera Wiki</span>,
+  darkMode: false,
+  primaryHue: 57,
+  chat: {
+    link: 'https://discord.gg/9cdEJVw3Tq',
+  },
+  project: {
+    link: 'https://readfameless.com',
+    icon: <svg width="24" height="24" viewBox="0 0 256 256"><path fill="currentColor" d="m231.9 169.8l-94.8 65.6a15.7 15.7 0 0 1-18.2 0l-94.8-65.6a16.1 16.1 0 0 1-6.4-17.3L45 50a12 12 0 0 1 22.9-1.1L88.5 104h79l20.6-55.1A12 12 0 0 1 211 50l27.3 102.5a16.1 16.1 0 0 1-6.4 17.3Z"></path></svg>,
+  },
+  faviconGlyph: <span>⨕</span>,
+  footer: {
+    text: <span>
+        <span>MIT {new Date().getFullYear()} © <a href="https://nextra.site" target="_blank">Nextra</a></span>
+        <span>TEST</span>
+        </span>,
+    }, 
+  // ...
+}
